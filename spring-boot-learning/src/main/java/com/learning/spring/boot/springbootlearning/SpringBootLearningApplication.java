dfe6e8913460bc9a62345d09f68f3a00621fdf60
Debnath Roy <Debnath@Debnaths-MacBook-Air.local>
2019-01-05 15:20:00 +0530

    First commit

---
+++ spring-boot-learning/src/main/java/com/learning/spring/boot/springbootlearning/SpringBootLearningApplication.java
@@ -0,0 +1,14 @@
+package com.learning.spring.boot.springbootlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningApplication.class, args);
+	}
+
+}
+
