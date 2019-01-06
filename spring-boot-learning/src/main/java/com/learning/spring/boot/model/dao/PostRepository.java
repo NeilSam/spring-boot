22976f715a0042205e88692b7a8370236a5a82f1
Debnath Roy <Debnath Roy@192.168.0.4>
2019-01-06 15:24:35 +0530

    .

--- spring-boot-learning/src/main/java/com/learning/spring/boot/model/dao/PostRepository.java
+++ spring-boot-learning/src/main/java/com/learning/spring/boot/model/dao/PostRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.learning.spring.boot.model.beans.Post;
-import com.learning.spring.boot.model.beans.User;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
